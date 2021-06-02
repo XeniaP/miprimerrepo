9d3aa0fd57554227ded97b6abcfc8c090dc17bc4
Yessenia Becerra <democloudconformity@gmail.com>
2021-06-02 13:43:29 -0500

    Mi primera API

---
+++ src/index.js
@@ -0,0 +1,22 @@
+var app = require("express")();
+const bodyParser = require("body-parser");
+var body = require("body-parser");
+const { response } = require("express");
+
+
+app.post("/get", bodyParser.raw({ type: 'application/json'}), (request, response) => {
+    var event = request.body
+    console.log(request.body.toString())
+
+    switch(event.type){
+        case 'ping':
+            console.log("PING")
+            break;
+        default:
+            console.log("default")
+    }
+
+    response.json({ received: true });
+});
+
+app.listen(8000, () => console.log("Running port 8000"));
